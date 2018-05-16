eebf81c58217a4d70ca51fd7f846b1482616e4f8
grandiloquent <1785907865@qq.com>
2018-05-16 10:34:58 +0800

    first commit

---
+++ SmartCurve/ImageCurve.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace SmartCurve
+{
+
+	public class ImageCurve
+	{
+		List<Point> keyPt = new List<Point>();
+		byte[] level = new byte[256];
+
+		public ImageCurve()
+		{
+		}
+		public byte[] GetImageLevel(Point[] pts )
+        {
+//            Point[] pts = new Point[keyPt.Count];
+//            for (int i = 0; i < pts.Length; i++)
+//            {
+//                pts[i].X = keyPt[i].X * 255 / this.Width;
+//                pts[i].Y = 255 - keyPt[i].Y * 255 / this.Height;
+//            }
+
+            for (int i = 0; i < pts[0].X; i++)
+                level[i] = (byte)pts[0].Y;
+            for (int i = pts[pts.Length - 1].X; i < 256; i++)
+                level[i] = (byte)pts[pts.Length - 1].Y;
+
+            Spline sp = new Spline();
+            sp.DataPoint = pts;
+            sp.Precision = 1.0;
+            Point[] spt=sp.SplinePoint;
+            for (int i = 0; i < spt.Length; i++)
+            {
+                int n = spt[i].Y;
+                if (n < 0) n = 0;
+                if (n > 255) n = 255;
+                level[pts[0].X + i] = (byte)n;
+            }
+            return level;
+        }
+
+		
+	}
+}
---
+++ SmartCurve/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+using System.Drawing;
+
+namespace SmartCurve
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+		
+			ApplyCurveFilter(@"C:\Users\Administrator\Desktop\新建文件夹\1.jpg");
+		}
+		
+		static void ApplyCurveFilter(string fileName)
+		{
+			Bitmap bmp= (Bitmap)Bitmap.FromFile(fileName);
+			var srcID = new ImageData();
+			srcID.FromBitmap(bmp);
+			var nch=srcID.Clone();
+			
+			var imageCurve=new ImageCurve();
+			var levelValue=imageCurve.GetImageLevel(new Point[]
+			                                        {
+			                                        	new Point(0,0),new Point(128,128),new Point(192,132),new Point(256,256)});
+			for (int w = 0; w < bmp.Width; w++) {
+				for (int h = 0; h < bmp.Height; h++) {
+//					if (red)
+//						nch.R[w, h] = levelValue[srcID.R[w, h]];
+//					if (green)
+//						nch.G[w, h] = levelValue[srcID.G[w, h]];
+//					if (blue)
+						nch.B[w, h] = levelValue[srcID.B[w, h]];
+				}
+			}
+			nch.ToBitmap().Save(@"C:\Users\Administrator\Desktop\新建文件夹\2.jpg",System.Drawing.Imaging.ImageFormat.Jpeg);
+		}
+	}
+}
